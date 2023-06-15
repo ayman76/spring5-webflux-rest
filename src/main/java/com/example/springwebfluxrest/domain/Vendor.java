2d1e02054b64f23a5f891c16529efde36d35621e
Ayman <a.m_ayman9@outlook.com>
2023-06-15 16:55:48 +0300

    Adding Vendor Class and its repository

---
+++ src/main/java/com/example/springwebfluxrest/domain/Vendor.java
@@ -0,0 +1,17 @@
+package com.example.springwebfluxrest.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document
+public class Vendor {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    
+}
