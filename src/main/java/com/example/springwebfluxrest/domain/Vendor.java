ebb79ace3bda2599ea4d987ac97d98cd9d599567
Ayman <a.m_ayman9@outlook.com>
2023-06-15 18:05:59 +0300

    Adding NoArgsConstructor, AllArgsConstructor, Builder annotations to Vendor and Category Classes

--- src/main/java/com/example/springwebfluxrest/domain/Vendor.java
+++ src/main/java/com/example/springwebfluxrest/domain/Vendor.java
@@ -3,9 +3,15 @@ package com.example.springwebfluxrest.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document
 public class Vendor {
 
